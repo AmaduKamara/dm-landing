5a3f9baa761be6c123f8383177c06e3f2ce8ba93
Amkam <amaduskamara@gmail.com>
2022-07-04 21:25:24 +0000

    Add initial content for support

--- src/components/Supports.jsx
+++ src/components/Supports.jsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import {PhoneIcon, ArrowRightIcon} from '@heroicons/react/outline'
+import React from "react";
+import { PhoneIcon, ArrowSmRightIcon } from "@heroicons/react/outline";
+import { ChipIcon, SupportIcon } from "@heroicons/react/solid";
+
+import supportBgImage from "../images/staff.jpg";
 
 const Supports = () => {
   return (
-    <div>Supports</div>
-  )
-}
+    <section>
+      <div>
+        <img src={supportBgImage} alt="Support Staff" />
+      </div>
+      <div>
+        <div>
+          <h2>Support</h2>
+          <h3>Finding the right team</h3>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit
+            accusamus quas quis perspiciatis vero quisquam, expedita hic ex
+            dicta vitae impedit nam repellendus, nisi tenetur magnam eligendi at
+            eos quibusdam?
+          </p>
+        </div>
 
-export default Supports
+        <div>
+          <div>
+            <div>
+              <PhoneIcon />
+              <h4>Sales</h4>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis
+              perspiciatis beatae ut cupiditate enim vitae impedit, nostrum quas
+              esse. Quam.
+            </div>
+            <div>
+              <p>
+                Contact Us <ArrowSmRightIcon />
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Supports;
