8eb486911f3bd24b26c39fc70aaae0453d8ee18f
Amkam <amaduskamara@gmail.com>
2022-07-04 21:16:08 +0000

    Create Supports component and import icons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from "./components/About";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Supports from "./components/Supports";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Supports />
     </div>
   );
 }
---
+++ src/components/Supports.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {PhoneIcon, ArrowRightIcon} from '@heroicons/react/outline'
+
+const Supports = () => {
+  return (
+    <div>Supports</div>
+  )
+}
+
+export default Supports
