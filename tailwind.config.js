b51e3419ae3fe48133080c0718e647400742d3e0
Amkam <amaduskamara@gmail.com>
2022-07-04 20:14:51 +0000

    Styled Navbar for mobile and desktop responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+
+      md: "768px",
+
+      lg: "1024px",
+
+      "2xl": "1536px",
+    },
   },
   plugins: [],
 };
