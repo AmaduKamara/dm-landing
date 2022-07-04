47963881bfad894d7bc2d4e7a3da11618f71fa7b
Amkam <amaduskamara@gmail.com>
2022-07-04 21:56:13 +0000

    Add AllInOne component with contents structures

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import About from "./components/About";
+import AllInOne from "./components/AllInOne";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Supports from "./components/Supports";
@@ -10,6 +11,7 @@ function App() {
       <Hero />
       <About />
       <Supports />
+      <AllInOne />
     </div>
   );
 }
---
+++ src/components/AllInOne.jsx
@@ -0,0 +1,31 @@
+import { CheckIcon } from "@heroicons/react/outline";
+import React from "react";
+
+const AllInOne = () => {
+  return (
+    <section>
+      <div>
+        <h2>All-In-One Platform</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Architecto
+          optio non id soluta hic! Aperiam repellat saepe optio perspiciatis
+          similique!
+        </p>
+        <div>
+          <div>
+            <CheckIcon className="w-7 mr-4 text-cyan-500" />
+            <div>
+              <h3>Notifications</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
+                pariatur eos porro sapiente eum non?
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AllInOne;
