c5d9aae07493679f7006347bd7d19315275554b6
Amkam <amaduskamara@gmail.com>
2022-07-04 21:05:55 +0000

    Add structure and content for about section

---
+++ src/components/About.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section>
+      <div className="container mx-auto">
+        <div>
+          <h2>Trusted by developers across the world</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
+            similique! Debitis pariatur delectus, temporibus laboriosam
+            voluptates praesentium veniam autem ad!
+          </p>
+        </div>
+        <div>
+          <div>
+            <h3>100%</h3>
+            <p>Completed</p>
+          </div>
+          <div>
+            <h3>24/7</h3>
+            <p>Delivery</p>
+          </div>
+          <div>
+            <h3>100K</h3>
+            <p>Transactions</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
